d63e4a6e660661213bf193913a3d44f1c7028af4
Riku-Riksa <rikvir11@gmail.com>
2020-02-06 16:21:24 +0200

    DOGGOS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Badge from 'react-bootstrap/Badge'
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup'
+
+
+import { useQuery} from '@apollo/react-hooks';
+import gql from "graphql-tag";
+
+const dogs = gql`
+{
+  dogs {
+    imageUrl
+    breed
+    color
+    name
+    description
+  }
+}
+`;
 
 function App() {
+  const {data, loading, error} = useQuery(dogs);
+
+  if (loading) return <img src="https://i.pinimg.com/originals/9e/0a/85/9e0a8567f01987492a3f23b43b69663d.gif" className="load"></img>
+  if (error) return <p>Error {error.message}</p>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {data && data.dogs.map((dog, i) => (
+      <div key = {i}>
+        <CardGroup style={{}}>
+        <Card bg="secondary" border="dark">
+          <Card.Img variant="top" src={dog.imageUrl}></Card.Img>
+          <Card.Body>
+            <Card.Title>{dog.name}</Card.Title>
+            <Card.Text>{dog.description}</Card.Text>
+          </Card.Body>
+          <Card.Footer>Breed: {dog.breed}</Card.Footer>
+          <Badge pill variant ="success">Good boy!</Badge>
+        </Card>
+        </CardGroup>
+      </div>
+    ))}
+    </>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { HttpLink } from 'apollo-link-http';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+
+const link = new HttpLink ({
+    uri: 'https://formidadog-ql.now.sh/graphql'
+})
+
+const client = new ApolloClient({
+    link,
+    cache
+})
+
+
+ReactDOM.render(<ApolloProvider client={client}><App  /></ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
